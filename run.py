4f0b520ee00622b8e2b3f5d9d9ac2ee05ffb7f89
abhishek <abhishekbhadoriya48@gmail.com>
2025-10-18 18:18:14 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
+"""
+Usage:
+1. Clone the repo.
+2. Activate your virtual environment with dependencies.
+3. Change the working directory to parent directory of this project.
+4. Run the following cmd:
+    `python run.py`
+"""
+
 from blur_suite.interactive import BlurSuiteApp
 
 app = BlurSuiteApp()
